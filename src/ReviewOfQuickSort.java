55f7ccbfd9f9047a9877cf6cc1fdec6d8a04811a
jommersu <jommer.su@foxmail.com>
2017-12-14 22:12:34 +0800

    add review

---
+++ src/ReviewOfQuickSort.java
@@ -0,0 +1,63 @@
+public class ReviewOfQuickSort {
+
+    public static void main(String[] args){
+        int[] arr = generate();
+        printArr(arr);
+        quickSort(arr);
+        printArr(arr);
+    }
+
+    public static void quickSort(int[] arr){
+        if(arr == null || arr.length < 2){
+            return;
+        }
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSort(int[] arr, int left, int right){
+        if(left < right){
+            int[] p = partition(arr, left, right);
+            quickSort(arr, left, p[0] - 1);
+            quickSort(arr, p[1] + 1, right);
+        }
+    }
+
+    public static int[] partition(int[] arr, int left, int right){
+        int less = left - 1;
+        int more = right;
+
+        while (left < more){
+            if(arr[left] < arr[right]){
+                swap(arr, ++less, left++);
+            }else if (arr[left] > arr[right]){
+                swap(arr, --more, left);
+            }else{
+                left++;
+            }
+        }
+        swap(arr, more , right);
+        return new int[] {less + 1, more};
+    }
+
+    public static void swap(int[] arr, int i ,int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static int[] generate(){
+        int[] arr = new int[(int) (5 * Math.random() + 5)];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = (int) (100 * Math.random());
+        }
+        return arr;
+    }
+
+    public static void printArr(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
