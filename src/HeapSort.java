25429b0c6f00363d9a9a2c0d1ae217be1eb9cd60
jommersu <jommer.su@foxmail.com>
2017-12-18 22:51:14 +0800

    add heapSort

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,2 +1,77 @@
 public class HeapSort {
+
+    public static void main(String[] args){
+        int[] arr = generate();
+        printArr(arr);
+        heapSort(arr);
+        printArr(arr);
+    }
+
+    public static void heapSort(int[] arr){
+        if(arr == null || arr.length < 2){
+            return;
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            heapInsert(arr, i);
+        }
+
+        int size = arr.length;
+        //swap(arr, 0, --size);
+        while (size > 0){
+            swap(arr, 0, --size);
+            heapify(arr, 0, size);
+            //swap(arr, 0, --size);
+        }
+
+    }
+
+    public static void heapInsert(int[] arr, int index){
+        while (arr[index] > arr[(index - 1) / 2]){
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    public static void heapify(int[] arr, int index, int size) {
+        int left = 2 * index + 1;
+        int right = left + 1;
+        while (left < size) {
+            int largest = right < size && arr[left] < arr[right] ? right : left;
+            largest = arr[largest] > arr[index] ? largest : index;
+
+            if (largest == index) {
+                break;
+            }
+
+            swap(arr, largest, index);
+            index = largest;
+            left = 2 * index + 1;
+            right = left + 1;
+        }
+    }
+
+    public static void swap(int[] arr, int i , int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static int[] generate(){
+        int[] arr = new int[(int)(5 * Math.random() + 5)];
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = (int)(50 * Math.random());
+        }
+        return arr;
+    }
+
+    public static void printArr(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+
 }
