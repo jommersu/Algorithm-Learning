9a060cba3c0432c70f0f091eac342afd7307657a
jommersu <jommer.su@foxmail.com>
2018-01-11 10:09:17 +0800

    add stack2

--- src/stack_and_queue/getMinStack.java
+++ src/stack_and_queue/getMinStack.java
@@ -10,15 +10,32 @@ import java.util.Stack;
  */
 public class getMinStack {
     public static void main(String[] args){
-        Stack1 stack_getMin = new Stack1();
-        stack_getMin.push(32);
-        stack_getMin.push(98);
-        stack_getMin.push(19);
-        stack_getMin.push(67);
+        Stack2 stack_getMin = new Stack2();
+        int[] arr = generateArr();
+//        int[] arr = {12,34,56,11,7,65};
+        for(int i = 0; i <arr.length; i++ ){
+            System.out.println("now push : " + arr[i]);
+            stack_getMin.push(arr[i]);
+        }
+
+        for(int i = arr.length - 1; i > 4; i--){
+            System.out.println("now pop : " + arr[i]);
+            stack_getMin.pop();
+        }
+
+        System.out.println("now the min number is : ");
 
         System.out.println(stack_getMin.getMin());
 
+    }
 
+    public static int[] generateArr(){
+        int[] arr = new int[10];
+        for(int i = 0; i < arr.length; i++ ){
+            arr[i] = (int)(50 * Math.random() + 2);
+        }
+
+        return arr;
     }
 }
 
@@ -60,4 +77,49 @@ class Stack1 {
         return this.minStack.peek();
     }
 }
+
+class Stack2 {
+    private Stack<Integer> dataStack;
+    private Stack<Integer> minStack;
+
+    public Stack2(){
+        this.dataStack = new Stack<Integer>();
+        this.minStack = new Stack<Integer>();
+    }
+
+    public void push(int num){
+
+        if(this.minStack.isEmpty()){
+            this.minStack.push(num);
+        }else if (num <= this.minStack.peek()){
+            this.minStack.push(num);
+        }
+
+        this.dataStack.push(num);
+    }
+
+    public int pop(){
+        if(this.dataStack.isEmpty()){
+            throw new RuntimeException("Your stack is empty");
+        }
+
+        int value = this.dataStack.pop();
+        if(value <= this.minStack.peek()){
+            this.minStack.pop();
+        }
+        return value;
+    }
+
+    public int getMin(){
+        if(this.minStack.isEmpty()){
+            throw new RuntimeException("Your stack is empty");
+        }
+
+        return this.minStack.peek();
+    }
+
+
+}
+
+
 
